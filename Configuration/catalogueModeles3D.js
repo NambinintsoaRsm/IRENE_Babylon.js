6c765c6e273fccc5e595599cedb8adced5226343
NambinintsoaRsm <nambinintsoa.rasoamanana@etud.u-picardie.fr>
2026-07-28 15:19:56 +0200

    correction catalogue

--- Configuration/catalogueModeles3D.js
+++ Configuration/catalogueModeles3D.js
@@ -9,16 +9,17 @@ import { Modele3D } from "../Domain/modele3d/Modele3D.js";
  */
 export const catalogueModeles3D = Object.freeze([
     new Modele3D({
-        id: "objet1",
-        nom: "Objet 1",
-        chemin: "assets/modeles/Objet-1/6005793941226.glb",
+        id: "Pot",
+        nom: "Pot",
+        chemin: "assets/modeles/pot/pot.glb",
         type: "glb"
     }),
 
-    new Modele3D({
-        id: "objet2",
-        nom: "Objet 2",
-        chemin: "assets/modeles/Objet-2/60057107471833.glb",
-        type: "glb"
-    })
+    new Modele3D(
+        {
+           id: "Pot cassé",
+           nom: "Pot cassé",
+            chemin: "assets/modeles/pot_casse/pot_cassé.glb",
+            type:"glb"
+        })
 ]);
